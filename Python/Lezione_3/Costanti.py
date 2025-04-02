cc096b3583a176c2e25c2a55690f382bb10d87fd
Because they can't C <paolo.procida@edu.immaginazioneelavoro.it>
2025-04-02 11:06:44 +0200

    Add files via upload

---
+++ Python/Lezione_3/Costanti.py
@@ -0,0 +1,9 @@
+# Le costanti sono delle variabili con valore immutabile
+# Hanno le stesse regole di denominazione delle variabili, tranne per il camelCase
+# Le costanti si dichiarano tutte in UPPERCASE
+
+mioNome = "Paolo"
+
+PI_GRECO = 3.14
+IVA = 0.22
+MINUTI_IN_ORA = 60
